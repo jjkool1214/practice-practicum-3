2cc4fff9b2a44f533d2c940b70ad0da7ffee0783
Oscar Capraro <oscar@mcapraro.com>
2023-11-11 14:00:12 -0500

    add practicum

---
+++ practicum.py
@@ -0,0 +1,18 @@
+"""
+Implement your solution to the practicum in this file.
+
+@author Oscar Capraro
+"""
+
+PHONETIC_ALPHABET = ["Alpha", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot",
+    "Golf", "Hotel", "India", "Juliett", "Kilo", "Lima", "Mike", "November", 
+    "Oscar", "Papa", "Quebec", "Romeo", "Sierra", "Tango", "Uniform", 
+    "Victor", "Whiskey", "X-ray", "Yankee", "Zulu"]
+
+def main():
+    # you may use this function to manually test your code if you feel the need
+    # to do so.
+    return
+
+if __name__ == "__main__":
+    main()
